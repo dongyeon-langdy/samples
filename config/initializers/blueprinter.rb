9bbf1f71d4ebbcb81f902a88728d12a3c550abaa
이동연 <dongyeon.lee@langdy.net>
2023-09-21 14:52:33 +0900

    reseting

--- config/initializers/blueprinter.rb
+++ config/initializers/blueprinter.rb
@@ -2,4 +2,6 @@ require 'oj' # you can skip this if OJ has already been required.
 
 Blueprinter.configure do |config|
   config.generator = Oj # default is JSON
+  config.field_default = nil
+  config.association_default = {}
 end
